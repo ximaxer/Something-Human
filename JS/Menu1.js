04ca4e94e320930cfc8f5f9a8fbb83e585a33a9c
ximaxer <53664220+ximaxer@users.noreply.github.com>
2020-05-24 22:43:17 +0100

    a
    a

--- JS/Menu1.js
+++ JS/Menu1.js
@@ -1,6 +1,6 @@
 class Menu1 extends Phaser.Scene {
     constructor(){
-        super("menu1");
+        super("Menu1");
     }
     preload(){
         this.load.image("play_button",("../resources/main_menu/buttons/button_default_base_blue_play.png"));
@@ -10,36 +10,56 @@ class Menu1 extends Phaser.Scene {
         this.load.image("options_button_dark",("../resources/main_menu/buttons/button_pressed_base_blue_options.png"));
         this.load.image("quit_button_dark",("../resources/main_menu/buttons/button_pressed_base_blue_quit.png"));
         this.load.image("menu_window",("../resources/main_menu/menu_window/menu_window.png"));
-        this.load.spritesheet("background","../resources/main_menu/menu_background/backgrounf_tileset.png", {frameWidth: 1280, frameHeight: 960});
+        this.load.spritesheet("background","../resources/main_menu/menu_background/background_tileset.png", {frameWidth: 1280, frameHeight: 960});
         this.load.spritesheet("menu_title","../resources/main_menu/menu_title/menu_title_tileset.png", {frameWidth: 811, frameHeight: 102});
     }
     create(){
+        this.anims.create({
+            key:"bg_anim",
+            frames: this.anims.generateFrameNumbers("background"),
+            frameRate: 11,
+            repeat: -1
+        });
         //image set up
-        this.background = this.add.spritesheet(0,0,"background");
+        this.background = this.physics.add.sprite(0,0,"background").setDepth(-2);
+        this.background.setScale(1);
+        this.background.setImmovable(true);
         this.background.setOrigin(0,0);
+        this.background.play("bg_anim");
 
-        this.play_button= this.add.image(360,245,'play_button');
-        this.play_button.setScale(0.09);
-        this.play_button_dark = this.add.image(360,245,'play_button_dark');
-        this.play_button_dark.setScale(0.38);
+        this.bg_window = this.add.image(500,200,"menu_window").setDepth(-1)
+        this.bg_window.setOrigin(0,0);
+
+        this.play_button= this.add.image(552,245,'play_button');
+        this.play_button.setScale(1);
+        this.play_button.setOrigin(0,0);
+        this.play_button_dark = this.add.image(552,245,'play_button_dark');
+        this.play_button_dark.setScale(1);
+        this.play_button_dark.setOrigin(0,0);
         this.play_button_dark.visible=false;
 
-        this.options_button = this.add.image(360,340,'options_button');
-        this.options_button.setScale(0.09);
-        this.options_button_dark = this.add.image(360,340,'options_button_dark');
-        this.options_button_dark.setScale(0.38);
+        this.options_button = this.add.image(552,340,'options_button');
+        this.options_button.setScale(1);
+        this.options_button.setOrigin(0,0);
+        this.options_button_dark = this.add.image(552,340,'options_button_dark');
+        this.options_button_dark.setScale(1);
+        this.options_button_dark.setOrigin(0,0);
         this.options_button_dark.visible=false;
 
-        this.quit_button= this.add.image(360,435,'quit_button');
-        this.quit_button.setScale(0.09);
-        this.quit_button_dark= this.add.image(360,435,'quit_button_dark');
-        this.quit_button_dark.setScale(0.38);
+        this.quit_button= this.add.image(552,435,'quit_button');
+        this.quit_button.setScale(1);
+        this.quit_button.setOrigin(0,0);
+        this.quit_button_dark= this.add.image(552,435,'quit_button_dark');
+        this.quit_button_dark.setScale(1);
+        this.quit_button_dark.setOrigin(0,0);
         this.quit_button_dark.visible=false;
 
-        this.quit_button = this.add.image(360,530,'quit_button');
-        this.quit_button.setScale(0.09);
-        this.quit_button_dark = this.add.image(360,530,'quit_button_dark');
-        this.quit_button_dark.setScale(0.38);
+        this.quit_button = this.add.image(552,530,'quit_button');
+        this.quit_button.setScale(1);
+        this.quit_button.setOrigin(0,0);
+        this.quit_button_dark = this.add.image(552,530,'quit_button_dark');
+        this.quit_button_dark.setScale(1);
+        this.quit_button_dark.setOrigin(0,0);
         this.quit_button_dark.visible=false;
 
     
@@ -59,7 +79,7 @@ class Menu1 extends Phaser.Scene {
         });
         this.play_button.on("pointerup", ()=>{
             this.game.canvas.style.cursor = "default";
-            this.scene.start("jogar1")
+            this.scene.start("Load")
             console.log("up jogar");
         });
 
